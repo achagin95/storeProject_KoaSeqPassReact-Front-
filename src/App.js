4a37b392b6f58a2e984d5da5452be6cee810a732
Andrey Chagin <a.chagin95@mail.ru>
2021-04-05 16:36:33 +0600

    0504 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './css/style.css';
 import { BrowserRouter } from 'react-router-dom';
 import { useRoutes } from './components/routes';
 import {NavBar} from './components/navbar/navbar';
+//import NavBar from './components/navbar/navbar'
 
 function App() {
   const routes = useRoutes()
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,30 +3,31 @@ import { NavLink } from 'react-router-dom'
 import { navItems } from './navItems'
 
 
-//class Navbar extends Component {
+//class NavBar extends Component {
 export const NavBar = () => {
+    //extends Component что значит, что дает))
     //render() {
-    return (
-        <nav className="NavbarItems">
-            <span className="navbar-logo">Store</span>
-            {/*можно попробовать в теге спан вместо h1*/}
-            <div className="menu-icon">
+        return (
+            <nav className="NavbarItems">
+                <span className="navbar-logo">Store</span>
+                {/*можно попробовать в теге спан вместо h1*/}
+                <div className="menu-icon">
 
-            </div>
-            <ul>
-                {navItems.map((item, index) => {
-                    return (
-                        <li key={index}>
-                            <NavLink to={`${item.url}`}>
-                                {item.title}
-                            </NavLink>
-                        </li>
-                    )
-                })}
-            </ul>
-        </nav>
-    )
-    // }
+                </div>
+                <ul>
+                    {navItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <NavLink to={`${item.url}`}>
+                                    {item.title}
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    //}
 }
 
-//export default Navbar
+//export default NavBar
